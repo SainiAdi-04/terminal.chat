9d4e04b2bec6b955861f40280f5770c36145c5b7
ad <sainiadi03@gmail.com>
2026-05-30 00:01:32 +0530

    refactored server

--- server.ts
+++ server.ts
@@ -1,55 +1,60 @@
 import type { ServerWebSocket } from "bun";
 
-interface messageInterface  {
+interface messageInterface {
     user: string
     text: string
 }
 
-type MyWebSocketData = {
-    username: string
-}
+type ClientData = {
+    username: string;
+};
 
-const clients = new Set<ServerWebSocket<MyWebSocketData>>(); 
+const clients = new Set<ServerWebSocket<ClientData>>();
 
 //ws is a person specific object created everytime someone connects and has a lot of capabilities. They're different objects even though they're both of type ServerWebSocket.
 
-const server: Bun.Server<MyWebSocketData> = Bun.serve({
-    fetch(req,server){
+Bun.serve<ClientData>({
+    fetch(req, server) {
         const url = new URL(req.url)
         const username = url.searchParams.get("username") ?? "Anon"
-        if (server.upgrade(req, {data: {username}})){
+        if (server.upgrade(req, { data: { username } })) {
             return;
         }
-        return new Response("Upgrade failed", {status:500});
+        return new Response("Upgrade failed", { status: 500 });
     },
     websocket: {
-        open(ws){
+        open(ws) {
             console.log("conencted!");
             ws.send("Welcome!")
             clients.add(ws);
-            for(const client of clients){
-                if (client !== ws){
+            for (const client of clients) {
+                if (client !== ws) {
                     client.send(`${ws.data.username} joined`)
                 }
             }
         },
         message(ws, message) {
-            console.log("Received:", message.toString());
-            const parsed= JSON.parse(message.toString()) as messageInterface
-            const formatted = `${parsed.user}: ${parsed.text}`
-            for (const client of clients){
-                if(client!==ws){
-                    client.send(formatted)
+            try {
+                console.log("Received:", message.toString());
+                const parsed = JSON.parse(message.toString()) as messageInterface
+                const formatted = `${parsed.user}: ${parsed.text}`
+                for (const client of clients) {
+                    if (client !== ws) {
+                        client.send(formatted)
+                    }
                 }
+            } catch (error) {
+                ws.send("Invalid message format");
             }
+
         },
-        close(ws, code, message){
+        close(ws) {
             clients.delete(ws)
-            for (const client of clients){
+            for (const client of clients) {
                 client.send(`${ws.data.username} left`)
             }
             console.log("client disconnected")
         },
-        drain(ws){}
+        drain(ws) { }
     }
 });
